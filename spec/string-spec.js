3103f86d512b91afa2bf7a844c23f9acda4ed714
Ethan Nwankwo <emekanwankwo201@gmail.com>
2016-11-19 11:23:06 +0100

    test(complete-test-setup): test cases creation complete.

--- spec/string-spec.js
+++ spec/string-spec.js
@@ -5,8 +5,6 @@ describe('String class', () => {
   describe('hasVowels', () => {
     it('Should return true if the string contains vowels', () => {
       expect('home'.hasVowels()).to.be.true;
-    });
-    it('Should return false if the string does not contain vowels', () => {
       expect('bcd'.hasVowels()).to.be.false;
     });
   });
@@ -32,8 +30,6 @@ describe('String class', () => {
   describe('isQuestion', () => {
     it('Should return true if the string is a question', () => {
       expect('Are you done?'.isQuestion()).to.be.true;
-    });
-    it('Should return false if the string is not a question', () => {
       expect('I am done'.isQuestion()).to.be.false;
     });
   });
@@ -41,6 +37,63 @@ describe('String class', () => {
   describe('words', () => {
     it('Should return a list of the words in the string as an array', () => {
       expect('This is a sentence'.words()).to.equal(['This', 'is', 'a', 'sentence']);
+    });
+  });
+
+  describe('wordCount', () => {
+    it('Should return the number of words in the string', () => {
+      expect('word'.wordCount()).to.equal(4);
+    });
+  });
+
+  describe('toCurrency', () => {
+    it('Should return a currency representation of the string', () => {
+      expect('11111.11'.toCurrency()).to.equal('11,111.11');
+    });
+  });
+
+  describe('fromCurrency', () => {
+    it('Should return a number representation of the currency string', () => {
+      expect('11,111.11'.fromCurrency()).to.equal(11111.11);
+    });
+  });
+
+  describe('inverseCase', () => {
+    it('Should return a string where each letter of the string as an inverse of its current case', () => {
+      expect('Mr. Ben'.inverseCase()).to.equal('mR. bEN');
+    });
+  });
+
+  describe('alternatingCase', () => {
+    it('Should return the letters in alternating cases starting with small letter', () => {
+      expect('Onomatopoeia'.alternatingCase()).to.equal('oNoMaToPoEiA');
+    });
+  });
+
+  describe('getMiddle', () => {
+    it('Should return the character(s) at the middle of the string', () => {
+      expect('read'.getMiddle()).to.equal('ea');
+      expect('reads'.getMiddle()).to.equal('a');
+    });
+  });
+
+  describe('numberWords', () => {
+    it('Should return the numbers in words', () => {
+      expect('325'.numberWords()).to.equal('three two five');
+    });
+  });
+
+  describe('isDigit', () => {
+    it('Should return true if the number is a digit and false otherwise', () => {
+      expect('3'.isDigit()).to.be.true;
+      expect('34'.isDigit()).to.be.false;
+    });
+  });
+
+  describe('doubleCheck', () => {
+    it('Should return true if the string contains double characters', () => {
+      expect('hello'.doubleCheck()).to.be.true;
+      expect('world'.doubleCheck()).to.be.false;
     });
   });
 });
