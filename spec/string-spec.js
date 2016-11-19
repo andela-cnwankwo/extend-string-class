e26d96ff432df76c42811707c8697dd6c6ddfb07
Ethan Nwankwo <emekanwankwo201@gmail.com>
2016-11-19 10:51:41 +0100

    test(isquestion-words-test): create tests for isQuestion and words methods.

--- spec/string-spec.js
+++ spec/string-spec.js
@@ -25,7 +25,22 @@ describe('String class', () => {
 
   describe('ucFirst', () => {
     it('Should return the string with the first character in upper case', () => {
-      expect('home'.ucfirst()).to.equal('Home');
+      expect('home'.ucFirst()).to.equal('Home');
+    });
+  });
+
+  describe('isQuestion', () => {
+    it('Should return true if the string is a question', () => {
+      expect('Are you done?'.isQuestion()).to.be.true;
+    });
+    it('Should return false if the string is not a question', () => {
+      expect('I am done'.isQuestion()).to.be.false;
+    });
+  });
+
+  describe('words', () => {
+    it('Should return a list of the words in the string as an array', () => {
+      expect('This is a sentence'.words()).to.equal(['This', 'is', 'a', 'sentence']);
     });
   });
 });
