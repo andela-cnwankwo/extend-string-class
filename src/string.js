d449b553a6e1a93b5e9235e63ec129f7b60bc484
Ethan Nwankwo <emekanwankwo201@gmail.com>
2016-11-21 19:43:17 +0100

    refactor(toupper-method): refactor toupper method, add tolower method.

--- src/string.js
+++ src/string.js
@@ -7,12 +7,24 @@ const ExtendString = {
     let cap = '';
     for (let i = 0; i < len; i += 1) {
       if (this[i].match(/[a-z]/)) {
-        cap += String.fromCharCode((65 + this.charCodeAt(i)) - 97);
+        cap += String.fromCharCode(this.charCodeAt(i) - 32);
       } else {
         cap += this[i];
       }
     }
     return cap;
+  },
+  toLower() {
+    const len = this.length;
+    let lower = '';
+    for (let i = 0; i < len; i += 1) {
+      if (this[i].match(/[A-Z]/)) {
+        lower += String.fromCharCode(this.charCodeAt(i) + 32);
+      } else {
+        lower += this[i];
+      }
+    }
+    return lower;
   }
 };
 
